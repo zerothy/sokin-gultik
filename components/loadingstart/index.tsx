14bf29e96596a4c4b8989f352208e96b8c74ab60
zerothy <zxuyagaming@gmail.com>
2024-07-05 23:37:30 +0700

    add: animations intro

--- components/loadingstart/index.tsx
+++ components/loadingstart/index.tsx
@@ -3,17 +3,23 @@ import { loadingStart } from "./LoadingConstant"
 
 export default function LoadingStart(){
     return (
-        <div className="flex flex-row items-center space-x-2">
-            {loadingStart.map(({fileName, altName}, index) => (
-                <div key={index}>
-                    <Image 
-                        src={`/images/loadingstate/${fileName}`}
-                        alt={altName}
-                        width={`${index === 2 ? 100 : 60}`}
-                        height={`${index === 2 ? 100 : 60}`}
-                    />
-                </div>
-            ))}
+        <div className="flex flex-col">
+            <div className="flex flex-row items-center space-x-2 pt-5">
+                {loadingStart.map(({fileName, altName}, index) => (
+                    <div key={index}>
+                        <Image 
+                            src={`/images/loadingstate/${fileName}`}
+                            alt={altName}
+                            width={`${index === 2 ? 100 : 60}`}
+                            height={`${index === 2 ? 100 : 60}`}
+                            className={`${index === 2 ? '' : `shake-effect-2 delay-0.${index}s`}`}
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="pt-3 shake-effect">
+                Sokin Gultik's<br/>Production
+            </div>
         </div>
     )
 }
