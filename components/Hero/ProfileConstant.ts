c144de1758762874dfe55c28faf51d030133f472
zerothy <zxuyagaming@gmail.com>
2024-07-08 23:04:28 +0700

    feat: image is now clickable

--- components/Hero/ProfileConstant.ts
+++ components/Hero/ProfileConstant.ts
@@ -4,7 +4,7 @@ const profileConstant = [
         altName: "profile"
     },
     {
-        fileName: "https://images.unsplash.com/photo-1720155243642-313849a7027b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        fileName: "https://images.unsplash.com/photo-1720371300677-ba4838fa0678?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
         altName: "profile"
     },
     {
@@ -12,7 +12,7 @@ const profileConstant = [
         altName: "profile"
     },
     {
-        fileName: "https://images.unsplash.com/photo-1720155243642-313849a7027b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        fileName: "https://images.unsplash.com/photo-1720206811364-684e8f8e803f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         altName: "profile"
     },
     {
@@ -20,11 +20,11 @@ const profileConstant = [
         altName: "profile"
     },
     {
-        fileName: "https://images.unsplash.com/photo-1720155243642-313849a7027b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        fileName: "https://images.unsplash.com/photo-1720206811364-684e8f8e803f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         altName: "profile"
     },
     {
-        fileName: "https://images.unsplash.com/photo-1720155243642-313849a7027b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        fileName: "https://images.unsplash.com/photo-1720206811364-684e8f8e803f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         altName: "profile"
     },
 ]
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 import { profileConstant } from "./ProfileConstant"
 
@@ -7,20 +7,46 @@ export default function Hero(){
     const [percentage, setPercentage] = useState(0)
     const [prevPercentage, setPrevPercentage] = useState(0)
     const [nextPercentage, setNextPercentage] = useState(0)
+    
+    const [selectedImage, setSelectedImage] = useState<number | null>(null)
+
+    const [anyImage, setAnyImage] = useState(true)
+
+    const handleSelectImage = (index: number) => {
+        setSelectedImage(index)
+        setAnyImage(true)
+    }
+
+    const handleDeselectImage = () => {
+        setAnyImage(false)
+    }
+
+    const handleLeft = () => {
+        setSelectedImage((prev) => {
+            if(prev === null) return profileConstant.length - 1
+            if(prev === 0) return profileConstant.length - 1
+            return prev - 1
+        })
+    }
+
+    const handleRight = () => {
+        setSelectedImage((prev) => {
+            if(prev === null) return 0
+            if(prev === profileConstant.length - 1) return 0
+            return prev + 1
+        })
+    }
 
     const win = typeof window !== "undefined" ? window : null
-
-    if (win) {
+    if(win){
+        // Desktop
         win.onmousedown = (e) => {
             setTrack(e.clientX)
         }
-
-        
         win.onmouseup = () => {
             setTrack(0)
             setPrevPercentage(percentage)
         }
-
         win.onmousemove = (e) => {
             if(track === 0) return
 
@@ -32,15 +58,14 @@ export default function Hero(){
             setPercentage(nextPercentage)
         }
 
+        // Mobile
         win.ontouchstart = (e) => {
             setTrack(e.touches[0].clientX)
         }
-
         win.ontouchend = () => {
             setTrack(0)
             setPrevPercentage(percentage)
         }
-
         win.ontouchmove = (e) => {
             if(track === 0) return
 
@@ -54,9 +79,9 @@ export default function Hero(){
     }
 
     return (
-        <div className="min-h-screen bg-charcoal" >
+        <div className="min-h-screen bg-charcoal">
             <div 
-                className="flex flex-row gap-4 absolute left-1/2 top-1/2 w-full" 
+                className="flex flex-row gap-4 absolute left-1/2 top-1/2 w-max" 
                 style={{
                     transform: `translate(${nextPercentage}%, -50%)`,
                     transition: "transform 1200ms",
@@ -65,11 +90,11 @@ export default function Hero(){
             >
                 {
                     profileConstant.map((profile, index) => (
-                        <div key={index}>
+                        <div key={index} onClick={() => {handleSelectImage(index)}}>
                             <img 
                                 src={profile.fileName} 
                                 alt={profile.altName} 
-                                className="object-cover unselectable h-80 w-56" draggable="false" 
+                                className="object-cover unselectable h-96 w-64" draggable="false" 
                                 style={{
                                     objectPosition: `${nextPercentage + 100}% 50%`,
                                     transition: "object-position 1200ms",
@@ -80,6 +105,25 @@ export default function Hero(){
                     ))
                 }
             </div>
+
+            {
+                (selectedImage !== null) && (
+                    <div 
+                        className={`z-40 absolute ${anyImage ? 'slideUp' : 'slideDown'}`}
+                    >
+                        <img
+                            src={profileConstant[selectedImage].fileName}
+                            alt={profileConstant[selectedImage].altName}
+                            className="object-cover unselectable h-screen w-screen" draggable="false"
+                        />
+                        <div>
+                            <div className="bg-blue-600 opacity-30 z-60 absolute w-[90vh] h-screen left-0 top-0" onClick={handleLeft}></div>
+                            <div className="bg-yellow-600 opacity-30 z-60 absolute w-[41vh] h-screen left-[90vh] top-0" onClick={handleDeselectImage}></div>
+                            <div className="bg-red-600 opacity-30 z-60 absolute w-[90vh] h-screen right-0 top-0" onClick={handleRight}></div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
