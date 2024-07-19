a44f7310d288cff55e597bf44503c60e997fd111
zerothy <zxuyagaming@gmail.com>
2024-07-19 15:28:46 +0700

    fix: boolean fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from "@/components/Hero";
 import useWindowWidth from "@/hooks/useWindowWidth";
 
 export default function Home() {
-    const [showIntro, setShowIntro] = useState(false);
+    const [showIntro, setShowIntro] = useState(true);
     const [music, setMusic] = useState(false);
     const windowWidth = useWindowWidth(); // Use the custom hook
 
