168bf0cae317a2c0fb7f16da1bf7e2bf72f36a1a
zerothy <zxuyagaming@gmail.com>
2024-07-21 22:43:32 +0700

    add: mobile restriction

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     const [showIntro, setShowIntro] = useState(true);
     const [music, setMusic] = useState(false);
     const [soundplay, setSoundplay] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+    const [isClient, setIsClient] = useState(false);
     const windowWidth = useWindowWidth();
 
     const handleIntroEnd = () => {
@@ -31,30 +33,45 @@ export default function Home() {
         if (music) sound.play();
     }, [music]);
 
+    useEffect(() => {
+        const checkIsMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        setIsMobile(checkIsMobile);
+        setIsClient(true);
+    }, []);
+
+
     const isWiderScreen = windowWidth >= 1024;
+
+    if(!isClient) return null;
 
     return (
         <div className="bg-charcoal">
-            {!isWiderScreen ? (
-                <div className="flex justify-center items-center h-screen w-screen">
-                    <h1 className="text-center text-ecru">Please open this website using wider screen :D</h1>
-                </div>
-            ) : (
-                <>
-                    {showIntro && <Intro onEnd={handleIntroEnd} musicStart={handleMusicStart} />}
-                    {!showIntro && <Hero />}
-                    {
-                        soundplay && (
-                            <div className={`absolute flex flex-row justify-center items-center text-xs text-ecru top-4 right-4 ${music ? 'opacity-100' : 'opacity-0'} transition-all duration-300 ease-out`}>
-                                <h1 className="mr-3 font-semibold">Daystar - Cozy Cafe</h1>
-                                <div className="w-10 h-10">
-                                    <video src="/videos/soundplay.mp4" loop autoPlay muted></video>
+            {
+                isMobile ? (
+                    <div className="flex justify-center items-center h-screen w-screen">
+                        <h1 className="text-center text-ecru">Please open this website using desktop or laptop :D</h1>
+                    </div>
+                ) : !isWiderScreen ? (
+                    <div className="flex justify-center items-center h-screen w-screen">
+                        <h1 className="text-center text-ecru">Please open this website using wider screen :D</h1>
+                    </div>
+                ) : (
+                    <>
+                        {showIntro && <Intro onEnd={handleIntroEnd} musicStart={handleMusicStart} />}
+                        {!showIntro && <Hero />}
+                        {
+                            soundplay && (
+                                <div className={`absolute flex flex-row justify-center items-center text-xs text-ecru top-4 right-4 ${music ? 'opacity-100' : 'opacity-0'} transition-all duration-300 ease-out`}>
+                                    <h1 className="mr-3 font-semibold">Daystar - Cozy Cafe</h1>
+                                    <div className="w-10 h-10">
+                                        <video src="/videos/soundplay.mp4" loop autoPlay muted></video>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    }
-                </>
-            )}
+                            )
+                        }
+                    </>
+                )
+            }
         </div>
     );
 }
