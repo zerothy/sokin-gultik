6f1c616aab7e2100ce7129680b109a0cdfcdaf48
zerothy <zxuyagaming@gmail.com>
2024-08-24 18:26:08 +0700

    add: more members

--- components/Hero/ProfileConstant.ts
+++ components/Hero/ProfileConstant.ts
@@ -14,11 +14,25 @@ const profileConstant = [
         quotes: "When life gets you down, you know what you gotta do. Just keep swimming."
     },
     {
+        fileName: "irfan.jpg",
+        altName: "irfan",
+        profileName: "Irfan",
+        fullName: "Irfan Rizqi Nurrahman",
+        quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
         fileName: "clara.jpg",
         altName: "clara",
         profileName: "Clara",
         fullName: "Clara Aurelia Setiady",
         quotes: '"Aku mau jadi orang kaya!"'
+    },
+    {
+        fileName: "kania.jpg",
+        altName: "kania",
+        profileName: "Kania",
+        fullName: "Kania Almyra Bilqist",
+        quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         fileName: "alex.jpg",
@@ -42,12 +56,26 @@ const profileConstant = [
         quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
+        fileName: "anggun.jpg",
+        altName: "anggun",
+        profileName: "Anggun",
+        fullName: "Anggun Sasmitha Dewi",
+        quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        fileName: "arvin.jpg",
+        altName: "arvin",
+        profileName: "Arvin",
+        fullName: "M. Arvin Wijayanto",
+        quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
         fileName: "jordan.jpg",
         altName: "jordan",
         profileName: "Jordan",
         fullName: "Muhammad Jordan Ar-Razi Aziz",
         quotes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    }
+    },
 ]
 
 export { profileConstant }
